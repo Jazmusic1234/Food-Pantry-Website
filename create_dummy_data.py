62f90f56f115e5e00090832620d8fcdee8eb8d25
Amy Nguyen <amy.yay22@gmail.com>
2019-10-26 13:33:34 -0700

    created python script for making dummy data

---
+++ create_dummy_data.py
@@ -0,0 +1,26 @@
+import json
+
+data = {}
+
+data['item'] = []
+
+data['item'].append({
+    'name': 'Brocolli',
+    'image': 'img/brocolli.jpg',
+    'credit': 3
+})
+
+data['item'].append({
+    'name': 'Banana',
+    'image': 'img/banana.jpg',
+    'credit': 4
+})
+
+data['item'].append({
+    'name': 'Bread',
+    'image': 'img/bread.jpg',
+    'credit': 3
+})
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
